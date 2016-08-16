188d5e652776187583111d90a861d1203dbf9922
weiz <weiz@weizs-MacBook-Pro.local>
2016-08-16 16:18:25 -0400

    karabiner update escape

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -2,16 +2,20 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.commandR2controlL_emacs 1
+$cli set option.vimode_hjkl 1
 /bin/echo -n .
-$cli set remap.shiftLshiftR_to_capslock 1
+$cli set private.head_tail 1
 /bin/echo -n .
-$cli set repeat.initial_wait 416
-/bin/echo -n .
-$cli set remap.delete2backslash 1
+$cli set private.word_move 1
 /bin/echo -n .
 $cli set remap.backslash2delete 1
 /bin/echo -n .
-$cli set option.vimode_hjkl 1
+$cli set remap.delete2backslash 1
+/bin/echo -n .
+$cli set remap.shiftL2shiftL_capslock 1
+/bin/echo -n .
+$cli set remap.shiftR2escape 1
+/bin/echo -n .
+$cli set repeat.initial_wait 416
 /bin/echo -n .
 /bin/echo
