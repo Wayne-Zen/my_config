a6d28262790e400575c720fe038c5fcb5133889d
weiz <weiz@weizs-MacBook-Pro.local>
2016-08-18 10:24:53 -0400

    update

---
+++ karabiner-import-mac-keyboard.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.vimode_hjkl 1
+/bin/echo -n .
+$cli set private.delete_word 1
+/bin/echo -n .
+$cli set private.head_tail 1
+/bin/echo -n .
+$cli set private.swap_backslash_delete 1
+/bin/echo -n .
+$cli set private.tab_visit 1
+/bin/echo -n .
+$cli set private.tab_visit_reverse 1
+/bin/echo -n .
+$cli set private.terminal_word_operation 1
+/bin/echo -n .
+$cli set private.word_delete 1
+/bin/echo -n .
+$cli set private.word_move 1
+/bin/echo -n .
+$cli set remap.commandRcommandL2escape 1
+/bin/echo -n .
+$cli set remap.shiftLshiftR_to_capslock 1
+/bin/echo -n .
+$cli set repeat.initial_wait 416
+/bin/echo -n .
+/bin/echo
